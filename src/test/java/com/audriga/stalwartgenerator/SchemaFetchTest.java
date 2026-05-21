6dde93eda6485c7d17972dd8ccfd11ec018a4c26
Luna Schwalbe <luna@audriga.com>
2026-05-21 13:33:58 +0200

    Run SchemaFetchTest against temporary container

--- src/test/java/com/audriga/stalwartgenerator/SchemaFetchTest.java
+++ src/test/java/com/audriga/stalwartgenerator/SchemaFetchTest.java
@@ -2,16 +2,26 @@ package com.audriga.stalwartgenerator;
 
 import okhttp3.HttpUrl;
 import org.junit.jupiter.api.Test;
-import rs.ltt.jmap.client.http.BearerAuthHttpAuthentication;
+import org.testcontainers.containers.GenericContainer;
+import rs.ltt.jmap.client.Services;
+import rs.ltt.jmap.client.http.BasicAuthHttpAuthentication;
 
 import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SchemaFetchTest {
     @Test
     void fetch() throws IOException {
-        var schema = JmapStalwartGenerator.fetchSchema(
-                InsecureX509TrustManager.HTTP_CLIENT,
-                HttpUrl.get("https://stalwart.luna.test"),
-                new BearerAuthHttpAuthentication("admin", "API_AAAAAQAAAAJNCmLgsT9TbFgq0wUacb1nM5iqQA"));
+        try (var stalwart = new GenericContainer<>("stalwartlabs/stalwart:v0.16.5")
+                .withExposedPorts(8080)
+                .withEnv("STALWART_RECOVERY_ADMIN", "admin:pw")) {
+            stalwart.start();
+            var schema = JmapStalwartGenerator.fetchSchema(
+                    Services.okHttpClient(),
+                    HttpUrl.get("http://%s:%d".formatted(stalwart.getHost(), stalwart.getFirstMappedPort())),
+                    new BasicAuthHttpAuthentication("admin", "pw"));
+            assertTrue(schema.objects().containsKey("x:Account"));
+        }
     }
 }
